01b763384232565dcff68828355c309c35b90cdc
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-19 07:09:27 -0600

    chatbot actualizado

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -1,9 +1,9 @@
-// Get the necessary elements from the DOM
+
 const sendButton = document.getElementById('send-btn');
 const chatInput = document.getElementById('chat-input');
 const chatBoxBody = document.getElementById('chatbox-body');
 
-// Function to add the user's message to the chatbox
+
 function addUserMessage(message) {
   const userMessageElement = document.createElement('div');
   userMessageElement.classList.add('message', 'user');
@@ -11,7 +11,7 @@ function addUserMessage(message) {
   chatBoxBody.appendChild(userMessageElement);
 }
 
-// Function to add the bot's message to the chatbox
+
 function addBotMessage() {
   const botMessageElement = document.createElement('div');
   botMessageElement.classList.add('message', 'bot');
@@ -19,22 +19,20 @@ function addBotMessage() {
   chatBoxBody.appendChild(botMessageElement);
 }
 
-// Event listener for the "Send" button
 sendButton.addEventListener('click', function() {
   const userMessage = chatInput.value;
 
-  // Check if the input is not empty
   if (userMessage.trim() === '') return;
 
-  // Add the user's message
+  
   addUserMessage(userMessage);
 
-  // Clear the input field
+ 
   chatInput.value = '';
 
-  // Add a bot's response
+
   addBotMessage();
 
-  // Scroll to the latest message
+ 
   chatBoxBody.scrollTop = chatBoxBody.scrollHeight;
 });
